42e65b83e93f5d5573543563ed3321ebe8bb749d
Rahul12071999 <rahulbodyal123@gmail.com>
2023-12-13 00:33:00 +0530

    changed the title

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerOptions = {
     swaggerDefinition: {
       info: {
-        title: 'Node.js CRUD operations',
+        title: 'CRUD operations Node.js & MongoDB',
         version: '1.0.0',
       },
       servers: [
